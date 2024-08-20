b3d13ed361a403758b74dfa5feba2f380ea0431b
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-08-20 19:53:22 +0200

    authentication request in client

--- src/client.rs
+++ src/client.rs
@@ -1,21 +1,28 @@
-use proto::calculator_client::CalculatorClient;
+use proto::admin_client::AdminClient;
 use std::error::Error;
 
 pub mod proto {
 	tonic::include_proto!("calculator");
 }
 
+use tonic::{metadata::MetadataValue, transport::Channel, Request};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-	let url = "http://[::1]:50051";
-	let mut client = CalculatorClient::connect(url).await?;
+	let token: MetadataValue<_> = "Bearer some-secret-token".parse()?;
 
-	let req = proto::CalculationRequest { a: 4, b: 5 };
+	let channel = Channel::from_static("http://[::1]:50051").connect().await?;
+	let mut client = AdminClient::with_interceptor(channel, move |mut req: Request<()>| {
+        req.metadata_mut().insert("authorization", token.clone());
+        Ok(req)
+    });
+
+	let req = proto::GetCountRequest {};
 	let request = tonic::Request::new(req);
 
-	let response = client.add(request).await?;
+	let response = client.get_request_count(request).await?;
 
-	println!("Response: {:?}", response.get_ref().result);
+	println!("Response: {:?}", response.into_inner().count);
 
 	Ok(())
 }
